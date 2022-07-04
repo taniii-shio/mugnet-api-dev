6f47a58b3185f83057210ee2e095601280c899c2
Naoto Shioya <shioya.naoto17@gmail.com>
2022-07-04 17:35:34 +0900

    create user CRUD algorithm

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,52 @@
 import express, { Request, Response } from "express";
 
+import User from "../models/User";
+
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-  res.send("users test");
+// ユーザー情報の取得
+router.get("/:id", async (req: Request, res: Response) => {
+  try {
+    const user: any = await User.findById(req.params.id);
+    const { password, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// ユーザー情報の更新
+router.put("/:id", async (req: Request, res: Response) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("ユーザー情報が更新されました");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res
+      .status(403)
+      .json("あなたは自分のアカウントの時だけ情報を更新できます");
+  }
+});
+
+// ユーザー情報の削除
+router.delete("/:id", async (req: Request, res: Response) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("ユーザー情報が削除されました");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res
+      .status(403)
+      .json("あなたは自分のアカウントの時だけ情報を削除できます");
+  }
 });
 
 export default router;
