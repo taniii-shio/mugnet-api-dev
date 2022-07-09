aae948a20af79493f3566d4566f1b06348039f9b
Naoto Shioya <shioya.naoto17@gmail.com>
2022-07-09 12:30:40 +0900

    create posts algorithm

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -15,4 +15,48 @@ router.post("/", async (req: Request, res: Response) => {
   }
 });
 
+// 投稿の取得
+router.get("/:id", async (req: Request, res: Response) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    return res.status(200).json(post);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// 投稿の更新
+router.put("/:id", async (req: Request, res: Response) => {
+  try {
+    // メモ：型要検討
+    const post: any = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body,
+      });
+      return res.status(200).json("投稿を更新しました");
+    } else {
+      return res.status(403).json("あなたは他のユーザーの投稿を編集できません");
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// 投稿の削除
+router.delete("/:id", async (req: Request, res: Response) => {
+  try {
+    // メモ：型要検討
+    const post: any = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      return res.status(200).json("投稿を削除しました");
+    } else {
+      return res.status(403).json("あなたは他のユーザーの投稿を削除できません");
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 export default router;
