bd8f8d3c12851aa1a534984f872afec6a59497ca
Naoto Shioya <shioya.naoto17@gmail.com>
2022-07-11 15:05:04 +0900

    refactor users.ts

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -9,7 +9,7 @@ router.get("/:id", async (req: Request, res: Response) => {
   try {
     const user: any = await User.findById(req.params.id);
     const { password, ...other } = user._doc;
-    res.status(200).json(other);
+    return res.status(200).json(other);
   } catch (err) {
     return res.status(500).json(err);
   }
@@ -19,10 +19,10 @@ router.get("/:id", async (req: Request, res: Response) => {
 router.put("/:id", async (req: Request, res: Response) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
     try {
-      const user = await User.findByIdAndUpdate(req.params.id, {
+      await User.findByIdAndUpdate(req.params.id, {
         $set: req.body,
       });
-      res.status(200).json("ユーザー情報が更新されました");
+      return res.status(200).json("ユーザー情報が更新されました");
     } catch (err) {
       return res.status(500).json(err);
     }
@@ -37,8 +37,8 @@ router.put("/:id", async (req: Request, res: Response) => {
 router.delete("/:id", async (req: Request, res: Response) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
     try {
-      const user = await User.findByIdAndDelete(req.params.id);
-      res.status(200).json("ユーザー情報が削除されました");
+      await User.findByIdAndDelete(req.params.id);
+      return res.status(200).json("ユーザー情報が削除されました");
     } catch (err) {
       return res.status(500).json(err);
     }
