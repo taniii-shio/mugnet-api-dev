102fdf3c8220044f1fb31fe8ca2ee42df9bbe6e3
Naoto Shioya <shioya.naoto17@gmail.com>
2022-07-12 13:15:49 +0900

    create PostSchema

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -2,7 +2,11 @@ import mongoose, { Schema, Document } from "mongoose";
 
 export interface IPost extends Document {
   userId: string;
+  categoryTag: Number;
   desc: string;
+  postPicture: string;
+  comments: string[];
+  likes: string[];
 }
 
 const PostSchema: Schema = new mongoose.Schema(
@@ -11,10 +15,26 @@ const PostSchema: Schema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    categoryTag: {
+      type: Number,
+      required: true,
+    },
     desc: {
       type: String,
       required: true,
       max: 200,
+    },
+    postPicture: {
+      type: String,
+      default: "",
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+    likes: {
+      type: Array,
+      default: [],
     },
   },
   { timestamps: true }
