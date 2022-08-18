c9881f8ac8083ea5832332fed39314d6d95d6611
Naoto Shioya <shioya.naoto17@gmail.com>
2022-08-18 13:18:43 +0900

    fix auth/login api

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -18,13 +18,14 @@ router.post("/register", async (req: Request, res: Response) => {
 // ログイン
 router.post("/login", async (req: Request, res: Response) => {
   try {
-    const user = await User.findOne({ email: req.body.email });
+    const user: any = await User.findOne({ email: req.body.email });
     if (!user) return res.status(404).json("ユーザーが見つかりません");
+    const { password, ...other } = user._doc;
 
     const vailedPassowrd = req.body.password === user.password;
     if (!vailedPassowrd) return res.status(404).json("パスワードが違います");
 
-    return res.status(200).json(user);
+    return res.status(200).json(other);
   } catch (err) {
     return res.status(500).json(err);
   }
