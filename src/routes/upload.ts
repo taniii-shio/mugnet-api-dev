756020ad9f35fc0c5c890f1d2a812210d17a4df6
Naoto Shioya <shioya.naoto17@gmail.com>
2022-08-28 13:21:29 +0900

    change function

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,25 +1,59 @@
 import express, { Request, Response } from "express";
+const aws = require("aws-sdk");
 const multer = require("multer");
+const multerS3 = require("multer-s3");
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: "no destination",
-  // (req: any, res: any, cb: any) => {
-  //   cb(null, "/public/images");
-  // },
-  filename: (req: any, file: any, cb: any) => {
-    cb(null, file.originalname);
-  },
+const s3 = new aws.S3({
+  accessKeyId: process.env.S3_ACCESS_KEY,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: process.env.S3_BUCKET_REGION,
 });
-const upload = multer({ storage });
+
+const upload = (bucketName: string) =>
+  multer({
+    storage: multerS3({
+      s3,
+      bucket: bucketName,
+      metadata: (req: any, file: any, cb: any) => {
+        cb(null, { fieldname: file.fieldname });
+      },
+      key: (req: any, file: any, cb: any) => {
+        cb(null, Date.now() + file.originalname);
+      },
+    }),
+  });
+
+const uploadSingle = upload("mugnet-api-dev").single("file");
+
 // 画像アップロード用のAPI
-router.post("/", upload.single("file"), (req: Request, res: Response) => {
-  try {
-    return res.status(200).json("画像アップロードに成功しました。");
-  } catch (err) {
-    console.log(err);
-  }
+router.post("/", (req: any, res: Response, next: any) => {
+  uploadSingle(req, res, (err: any) => {
+    if (err) {
+      console.log("errors", err);
+      res.json({ error: err });
+    } else {
+      if (req.file === undefined) {
+        console.log("Error: No File Selected!");
+        res.json("Error: No File Selected");
+      } else {
+        // If Success
+        const imageName = req.file.key;
+        const imageLocation = req.file.location;
+        // Save the file name into database into profile model
+        res.json({
+          image: imageName,
+          location: imageLocation,
+        });
+      }
+    }
+  });
+  // try {
+  //   return res.status(200).json("画像アップロードに成功しました。");
+  // } catch (err) {
+  //   console.log(err);
+  // }
 });
 
 export default router;
